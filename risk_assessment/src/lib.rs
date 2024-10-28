5640d0ee6cb305e0fbb563e60d7389cf21d063f0
endre90 <endre90@gmail.com>
2024-10-28 17:27:22 +0100

    shannoning ticker rates didn't help, need and extra step

--- risk_assessment/src/lib.rs
+++ risk_assessment/src/lib.rs
@@ -1,7 +1,7 @@
 pub static EMULATOR_NODE_ID: &'static str = "risk_assessment_emulator";
 pub static NODE_ID: &'static str = "risk_assessment_runner";
 pub static TEST_TICKER_RATE: u64 = 1000; // milliseconds
-pub static CLIENT_TICKER_RATE: u64 = 100; // milliseconds
+pub static CLIENT_TICKER_RATE: u64 = 200; // milliseconds
 pub static NUMBER_OF_TEST_CASES: u64 = 20;
 
 pub mod emulators;
